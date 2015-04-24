2933dc98c09df76fb135c3a9904b7435a6b1e80b
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2015-04-25 00:08:40 +0200

    added new design
    Credit for water drop icon: http://www.freepik.com

--- Android/DiscoveryVehicleRemote/app/src/main/java/de/ohg/fitag/android/discoveryVehicleRemote/MainActivity.java
+++ Android/DiscoveryVehicleRemote/app/src/main/java/de/ohg/fitag/android/discoveryVehicleRemote/MainActivity.java
@@ -3,10 +3,6 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -14,11 +10,8 @@ import android.os.IBinder;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,16 +22,14 @@ import java.io.IOException;
 
 import android.content.ServiceConnection;
 
-public class MainActivity extends Activity implements SensorEventListener {
-    private ImageView image;
+public class MainActivity extends Activity {
 
-    private TextView tvHeading;
     private TextView tvConnection;
-    private ProgressBar pgSpinner;
     private Button btAction;
 
-    private float currentDegree = 0f;
-    private SensorManager mSensorManager;
+    private TextView tvWaterDepth;
+    private ImageView ivWaterFound;
+
     private ConnectionState connectionState;
 
     private boolean bound;
@@ -49,15 +40,17 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        image = (ImageView) findViewById(R.id.ivCompass);
-        tvHeading = (TextView) findViewById(R.id.tvHeading);
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
         tvConnection = (TextView) findViewById(R.id.tvConnectionState);
-        pgSpinner = (ProgressBar) findViewById(R.id.pgSpinner);
         btAction = (Button) findViewById(R.id.btAction);
+
+        tvWaterDepth = (TextView) findViewById(R.id.tvWaterDepth);
+        ivWaterFound = (ImageView) findViewById(R.id.ivWaterFound);
+
         connectionState = ConnectionState.DISCONNECTED;
+
         setupNXJCache();
+
+        foundWater(-1);
 
         final Button button = (Button) findViewById(R.id.btAction);
         button.setOnClickListener(new View.OnClickListener() {
@@ -71,6 +64,7 @@ public class MainActivity extends Activity implements SensorEventListener {
                         break;
                     default:
                         backgroundService.openConnection();
+                        foundWater(-1);
                 }
             }
         });
@@ -88,30 +82,44 @@ public class MainActivity extends Activity implements SensorEventListener {
                 public void run() {
                     tvConnection.setTextColor(connectionState.color());
                     tvConnection.setText(getResources().getText(connectionState.value()));
+
+                    foundWater(-1);
                 }
             });
         }
 
         @Override
-        public void onWaterFound(float depth) {
-
+        public void onWaterFound(final float depth) {
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                   foundWater(depth);
+                }
+            });
         }
     };
+
+    public void foundWater(float depth){
+        if(depth == -1){
+            tvWaterDepth.setEnabled(false);
+            ivWaterFound.setImageDrawable(getResources().getDrawable(R.drawable.water_drop_inactive));
+        }else{
+            tvWaterDepth.setEnabled(true);
+            tvWaterDepth.setText("Tiefe: "+Math.round(depth));
+            ivWaterFound.setImageDrawable(getResources().getDrawable(R.drawable.water_drop_active));
+        }
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
         final Intent serviceIntent = new Intent(this, LejosBackgroundService.class);
         bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE);
-        // for the system's orientation sensor registered listeners
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-                SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(this);
     }
 
     @Override
@@ -150,31 +158,6 @@ public class MainActivity extends Activity implements SensorEventListener {
         } catch (IOException e) {
             Log.e(TAG, "Could not write nxj.cache " + e.getMessage(), e);
         }
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        // get the angle around the z-axis rotated
-        float degree = Math.round(event.values[0]);
-        tvHeading.setText("Heading: " + Float.toString(degree) + " degrees");
-        // create a rotation animation (reverse turn degree degrees)
-        RotateAnimation ra = new RotateAnimation(
-                currentDegree,
-                -degree,
-                Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f);
-        // how long the animation will take place
-        ra.setDuration(210);
-        // set the animation after the end of the reservation status
-        ra.setFillAfter(true);
-        // Start the animation
-        image.startAnimation(ra);
-        currentDegree = -degree;
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
     private ServiceConnection connection = new ServiceConnection() {
